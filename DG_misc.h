d1e63fee882c108b6fe391db03ef5d1c3345d45a
Daniel Gibson <metalcaedes@gmail.com>
2017-07-05 13:54:02 +0200

    DG_misc.h: Add 1911 to _MSC_VER table for 15.3
    aka "Visual Studio 2017 Version 15.3 Preview" - yeah, great name..
    but maybe this is transitional and they'll drop the year from the name again?

--- DG_misc.h
+++ DG_misc.h
@@ -689,7 +689,8 @@ DG_MISC_DEF size_t DG_strnlen(const char* s, size_t n)
  * so I don't have to google that over and over again
  * (from http://sourceforge.net/p/predef/wiki/Compilers/#microsoft-visual-c)
  * Visual C++   _MSC_VER
- * 15.0 (2017)   1910 (at least "Preview 5", next Preview/Release might be 1911)
+ * 15.3 (2017)   1911 // "Visual Studio 2017 Version 15.3 Preview"
+ * 15.0 (2017)   1910
  * 14.0 (2015)   1900
  * 12.0 (2013)   1800
  * 11.0 (2012)   1700
